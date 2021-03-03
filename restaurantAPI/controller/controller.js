a4b6fe1e500a171b3dd33b5997f4ffa68191404a
samsom2017 <60977591+samsom2017@users.noreply.github.com>
2021-03-04 00:07:33 +0100

    Add files via upload

---
+++ restaurantAPI/controller/controller.js
@@ -0,0 +1,76 @@
+const Restaurant = require("../model/restaurantModel");
+
+// Get a simplified list of all restaurants
+exports.restaurantList = (req, res) => {
+  const sortBy = req.query.q;
+  Restaurant.find((err, item) => {
+    if (err) {
+      console.error(err);
+      res.status(400).end();
+      return;
+    }
+    res.status(200).json({ item });
+  });
+};
+
+// Get detailed information about a single restaurant
+exports.restaurantInfo = (req, res) => {
+  Restaurant.findOne({ id: req.params.id }, (err, item) => {
+    if (err) {
+      console.error(err);
+      res.status(400).end();
+      return;
+    } else if (!item) {
+      res.status(404).end();
+      return;
+    }
+    res.status(200).json({ item });
+  });
+};
+
+// Add a new restaurant to the DB
+exports.restaurantCreate = (req, res) => {
+  Restaurant.create(req.body, (err, item) => {
+    if (err) {
+      console.error(err);
+      res.status(400).end();
+      return;
+    }
+    res.status(201).json({ item });
+  });
+};
+
+// Delete a restaurant from the DB
+exports.restaurantDelete = (req, res) => {
+  Restaurant.findOneAndRemove({ id: req.params.id }, (err, item) => {
+    if (err) {
+      console.error(err);
+      res.status(400).end();
+      return;
+    } else if (!item) {
+      res.status(404).end();
+      return;
+    }
+    res.status(200).end();
+  });
+};
+
+// Update information about a restaurant
+exports.restaurantUpdate = (req, res) => {
+  Restaurant.findOneAndUpdate(
+    { id: req.params.id },
+    req.body,
+    {},
+    (err, item) => {
+      if (err) {
+        console.error(err);
+        res.status(400).end();
+        return;
+      } else if (!item) {
+        res.status(404).end();
+        return;
+      }
+      res.status(201).end();
+    }
+  );
+};
